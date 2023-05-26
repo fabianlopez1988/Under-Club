6d23e907a9a2041aebfd3a017e6cec1ea04c5b25
MatiasNasif <matiasezequielnasif@gmail.com>
2023-05-26 00:41:41 -0300

    feat: user and auth routes

--- api/src/auth/models/token.model.ts
+++
@@ -1,4 +0,0 @@
-/* export interface PayloadToken {
-  email: string;
-  sub: number;
-} */
--- api/src/users/dtos/user.dto.ts
+++ api/src/users/dtos/user.dto.ts
@@ -1,18 +1,7 @@
-import {
-  IsBoolean,
-  IsEmail,
-  IsEnum,
-  IsNotEmpty,
-  IsOptional,
-  IsString,
-} from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 import { PartialType } from '@nestjs/mapped-types';
-import mongoose from 'mongoose';
 
 export class CreateUserDto {
-  @IsString()
-  readonly fullName: string;
-
   @IsNotEmpty()
   @IsEmail()
   readonly email: string;
@@ -20,19 +9,6 @@ export class CreateUserDto {
   @IsNotEmpty()
   @IsString()
   readonly password: string;
-
-  @IsBoolean()
-  readonly admin: boolean = false;
-
-  @IsEnum(['Activo', 'Inactivo'])
-  @IsOptional()
-  readonly status?: string = 'Activo';
-
-  @IsOptional()
-  packages?: mongoose.Types.ObjectId[];
-
-  @IsOptional()
-  readonly photo?: string;
 }
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {}
