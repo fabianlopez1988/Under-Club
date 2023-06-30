2f0a59427a75433cc3ac9f113e0fdc577f46842e
fabian <fabianlopez1988@gmail.com>
2023-06-29 23:55:37 -0300

    fix: add data-rh=true to index.html for meta

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -7,48 +7,21 @@ import Podcast from "../Podcast/Podcast";
 import Episodes from "../Episodes/Episodes";
 
 import { motion } from "framer-motion";
-import { Helmet } from "react-helmet-async";
 
 const Home = () => {
   return (
-      <>
-      <Helmet>
-      {/* <!-- Primary Meta Tags --> */}
-      <title>Under Club</title>
-      <meta name="title" content="Under Club" />
-      <meta name="description"
-        content="Under Club pertenece al puñado de clubes que han sabido construir una identidad sonora en conjunto con su público. Desde Buenos Aires hacia todo el mundo." />
-
-      {/* <!-- Open Graph / Facebook --> */}
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://underclub.com.ar/" />
-      <meta property="og:title" content="Under Club" />
-      <meta property="og:description"
-        content="Under Club pertenece al puñado de clubes que han sabido construir una identidad sonora en conjunto con su público. Desde Buenos Aires hacia todo el mundo." />
-      <meta property="og:image" content="https://static.ra.co/images/clubs/ar-under-logo.jpg?dateUpdated=1678803496050" />
-
-      {/* <!-- Twitter --> */}
-      <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://underclub.com.ar/" />
-      <meta property="twitter:title" content="Under Club" />
-      <meta property="twitter:description"
-        content="Under Club pertenece al puñado de clubes que han sabido construir una identidad sonora en conjunto con su público. Desde Buenos Aires hacia todo el mundo." />
-      <meta property="twitter:image" content="https://static.ra.co/images/clubs/ar-under-logo.jpg?dateUpdated=1678803496050" />
-          
-        </Helmet>
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0, transition: { duration: 0.1 } }}
-        >
-          <Header />
-          <ShortHistory />
-          <CarouselHome />
-          <Podcast />
-          <Episodes />
-          <Footer />
-        </motion.div>
-      </>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0, transition: { duration: 0.1 } }}
+    >
+      <Header />
+      <ShortHistory />
+      <CarouselHome />
+      <Podcast />
+      <Episodes />
+      <Footer />
+    </motion.div>
   );
 };
 
